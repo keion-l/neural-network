43f57a3b4ef7cf8025eade026bd45a558f134380
keion-l <keionlarsen@icloud.com>
2022-11-26 21:59:51 +1000

    create neuron class

--- neuron-example.py
+++ neuron-example.py
@@ -3,3 +3,15 @@ import numpy as np
 def sigmoid(x):
     # activation function (logisitic sigmoid function)
     return 1 / (1 + np.exp(-x))
+
+class Neuron:
+
+    def __init__(self, weights, bias):
+        self.weights = weights #array 
+        self.bias = bias
+
+
+    def feedforward(self, inputs):
+        #weight inputs and add the bias then user activation function
+        total = np.dot(self.weights, inputs) + self.bias #using dot product
+        return sigmoid(total)
