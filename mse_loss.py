e857f01c00c09cb021c41c9cc402281acfbc5848
keion-l <keionlarsen@icloud.com>
2022-11-27 15:17:29 +1000

    test mse loss func

--- mse_loss.py
+++ mse_loss.py
@@ -3,3 +3,8 @@ import numpy as np
 def mse_loss(y_true, y_pred):
     return ((y_true - y_pred) ** 2).mean()
 
+# example 
+y_true = np.array([1, 0, 1, 0])
+y_pred = np.array([0, 0, 0, 0])
+
+print(mse_loss(y_true, y_pred)) # 0.5
